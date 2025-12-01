52c08593cca19f460a69a04ccb0aa743ca243f6f
Kiwi-Eric-Li <lxf7756@126.com>
2025-12-01 17:00:03 +0800

    initial commit

---
+++ Controllers/TestAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeXiechengAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2", "value3" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace FakeXiechengAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // app.MapGet("/", () => "Hello World!");
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
